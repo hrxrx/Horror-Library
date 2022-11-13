37ba24eeb428696828c6d20cc0c1e57e98080eb9
Angel Reyna <74248282+hrxrx@users.noreply.github.com>
2022-11-13 18:16:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,136 @@
+let books = {
+    grozine: [
+      {
+        isbn: 9786098233346,
+        price: 7.99,
+        year: 2006,
+        title: "Bloga dukte",
+        pagecount: 250
+      },
+      {
+        isbn: 9786098233391,
+        price: 7.99,
+        year: 2015,
+        title: "Mergina kuria jis pazinojo",
+        pagecount: 315
+      },
+      {
+        isbn: 9786099609324,
+        price: 7.99,
+        year: 2019,
+        title: "Tapk ragana",
+        pagecount: 150
+      },
+      {
+        isbn: 9786094792250,
+        price: 6.99,
+        year: 2007,
+        title: "Sfera",
+        pagecount: 450
+      },
+      {
+        isbn: 9786094792335,
+        price: 9.99,
+        year: 2019,
+        title: "Mes susitinkame cia",
+        pagecount: 500
+      }
+    ],
+    scifi: [
+      {
+        isbn: 9786094273780,
+        price: 7.99,
+        year: 2019,
+        title: "Sunaikinimas",
+        pagecount: 509
+      },
+      {
+        isbn: 9786098233483,
+        price: 12.99,
+        year: 2018,
+        title: "Artemide",
+        pagecount: 199
+      },
+      {
+        isbn: 9786094273872,
+        price: 4.99,
+        year: 2015,
+        title: "Fondas ir imperija",
+        pagecount: 185
+      },
+      {
+        isbn: 9786094273902,
+        price: 7.99,
+        year: 2019,
+        title: "Amzinybes fjordo pranasai",
+        pagecount: 333
+      }
+    ],
+    'skandinaviski detektyvai': [
+      {
+        isbn: 9786094442742,
+        price: 5.99,
+        year: 2004,
+        title: "Bejegiai",
+        pagecount: 777
+      },
+      {
+        isbn: 9786094442940,
+        price: 14.99,
+        year: 2019,
+        title: "Klajunai",
+        pagecount: 172
+      },
+      {
+        isbn: 9786090404386,
+        price: 7.99,
+        year: 2015,
+        title: "Mergina, kuri pakliuvo i voratinkli",
+        pagecount: 356
+      }
+    ]
+  }
+  
+
+  //////
+
+  console.log(books)
+
+
+
+  // 1.1 Išvesti knygas, kurios išleistos 2018 m.
+
+
+function output(key,title,year,isbn,pages,price) {
+  console.log('Kategorija:',key,'\n'+
+    'Pavadinimas:',title,'\n'+
+    'Metai:',year,'\n'+
+    'ISBN:',isbn,'\n'+
+    'Puslapiu skaicius:',pages,'\n'+
+    'Kaina:',price)
+}
+
+for(var key in books) { // einam per žanrus
+var category = books[key]; // rasti žanrai
+
+// ieškom tarp knygų 2018 metų
+const x = category.find(({ year }) => year == 2018);
+if(x) { // jeigu rado
+  output(key,x.title,x.year,x.isbn,x.pages,x.price);
+}
+
+
+
+
+
+  // 1.2 Išvesti knygų kategorijas su pigiausiomis knygomis.
+
+
+
+// kiekvienam žanre ieškom pigiausios knygos
+const y = category.reduce(( prev, curr ) => prev.price < curr.price ? prev : curr)
+if(y) { // jeigu rado
+  output(key,y.title,y.year,y.isbn,y.pages,y.price);
+}
+}
+
